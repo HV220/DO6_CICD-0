23bf24e200b87c13f5d345befa9b1b8e4e1f28b9
aladales.VM <aladales_VM>
2022-11-22 22:21:19 +0000

    fsdfsd

--- src/helpers/ssh-copy.sh
+++ src/helpers/ssh-copy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash/
 
-scp ../C3_SimpleBashUtils-0/src/cat ../C3_SimpleBashUtils-0/src/grep aladales@10.100.0.11:/home/aladales/
-echo "1"
-ssh aladales@10.100.0.11 sudo -A mv s21_cat s21_grep /usr/local/bin/
-echo "1"
+scp ../C3_SimpleBashUtils-0/src/cat ../C3_SimpleBashUtils-0/src/grep aladales1@10.100.0.10:/home/aladales/
+ssh aladales1@10.100.0.10 sudo -A mv s21_cat s21_grep /usr/local/bin/
