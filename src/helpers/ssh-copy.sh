a7f81d180a9461a53d064185172ea4da3d0cbece
korpo <diagenin@gmail.com>
2022-11-22 23:39:57 +0300

    sfsdf

--- src/helpers/ssh-copy.sh
+++ src/helpers/ssh-copy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash/
 
-scp ../C3_SimpleBashUtils-0/src/cat ../C3_SimpleBashUtils-0/src/grep aladales@127.0.0.1:/home/aladales/
-ssh aladales@127.0.0.1 sudo -A mv s21_cat s21_grep /usr/local/bin/
+scp ../C3_SimpleBashUtils-0/src/cat ../C3_SimpleBashUtils-0/src/grep aladales@10.100.0.11:/home/aladales/
+ssh aladales@10.100.0.11 sudo -A mv s21_cat s21_grep /usr/local/bin/
